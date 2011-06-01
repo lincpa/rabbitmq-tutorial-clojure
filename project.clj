730fedd0e777dec7276709b685460a8fd015d87c
rippinrobr <rippinrobr@hotmail.com>
2011-05-31 20:35:22 -0400

    added a few finishing touches and now I'm done.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Tutorial: A clojure version of the RabbitMQ tutorials"
   :dependencies [[com.mefesto/wabbitmq "0.1.4"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/clojure "1.2.1"]])
+                 [org.clojure/clojure "1.2.1"]]
+  :main tutorial.core)
